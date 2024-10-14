f47cd30855413242810e20fff211ce2fc580e4d8
Wilmer Sarpec <sarpecwilmer7@gmail.com>
2024-10-13 18:46:50 -0600

    tienda ferreteria

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  apiURL: 'https://ferreteria-api.onrender.com/api/v6/productos/findall',
+  stripeAPIKey: '',
+  serverURL: 'https://ferreteria-api.onrender.com/api/v6/productos/findall',
+};
